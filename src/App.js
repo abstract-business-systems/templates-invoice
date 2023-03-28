310928946e09defff2afb6704e9a545d1a726e88
rkr-abs <rkr@abstractit.in>
2023-03-28 13:23:41 +0530

    Introduce MyDocument component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { React } from 'react';
 import './App.scss';
+import MyDocument from './components/MyDocument';
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 
 const App = () =>
 	<div className="App">
-		Ready to start.
+		<PDFViewer><MyDocument/></PDFViewer>
+		<PDFDownloadLink document={ <MyDocument/> } fileName="somename.pdf">
+			{({ loading }) =>
+				(loading ? 'Loading document...' : 'Download now!')}
+		</PDFDownloadLink>
 	</div>;
 
 export default App;
