2f8bd9bc06801045ea528143d604ad42e9c4b446
rkr-abs <rkr@abstractit.in>
2023-04-03 15:51:38 +0530

    Using invoiceNo and invoiceDate from config.

--- src/components/InvoiceSection.js
+++ src/components/InvoiceSection.js
@@ -18,15 +18,19 @@ const styles = StyleSheet.create({
 	},
 });
 
-const InvoiceSection = () => <View style={ styles.view }>
-	<Text style={ styles.subtext }>
-		<Text style={ styles.bold }>Invoice No:</Text>
-		<Text>  13-06-2001</Text>
-	</Text>
-	<Text style={ styles.subtext }>
-		<Text style={ styles.bold }>Invoice Date:</Text>
-		<Text>  13-06-2001</Text>
-	</Text>
-</View>;
+const InvoiceSection = (context) => {
+	const { config: { obj: { invoiceNo, invoiceDate }}} = context;
+
+	return <View style={ styles.view }>
+		<Text style={ styles.subtext }>
+			<Text style={ styles.bold }>Invoice No:</Text>
+			<Text>  {invoiceNo}</Text>
+		</Text>
+		<Text style={ styles.subtext }>
+			<Text style={ styles.bold }>Invoice Date:</Text>
+			<Text>  {invoiceDate}</Text>
+		</Text>
+	</View>;
+};
 
 export default InvoiceSection;
