29acf192233ae00b5152cf5853c03cebc3be4f96
rkr-abs <rkr@abstractit.in>
2023-04-04 16:58:02 +0530

    add RobotoMono fonts.

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Text, View } from '@react-pdf/renderer';
 const subtext = {
-	width: '80%',
+	width: '90%',
 	fontSize: 10,
 	fontFamily: 'Roboto-Light',
-	letterSpacing: '1px',
 };
 const Description = () =>
 	<View style={ {
