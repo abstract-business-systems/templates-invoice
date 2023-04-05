dd30f098b9f4e9e90e932ca999db02195bdedf36
rkr-abs <rkr@abstractit.in>
2023-04-05 09:12:56 +0530

    Introduced ClientAddress component.

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import { View, Text } from '@react-pdf/renderer';
+import { View } from '@react-pdf/renderer';
 import VendorAddress from './VendorAddress';
-
-const subtext = {
-	marginLeft: 99,
-	paddingLeft: 30,
-	fontSize: 10,
-	width: 180,
-	flexWrap: 'wrap',
-};
+import ClientAddress from './ClientAddress';
 
 const Address = (context) => {
-	const { config: { obj: { client }}, data: styles } = context;
+	const { data: styles } = context;
 
 	return <View style={ styles.view }>
 		<VendorAddress { ...context }/>
-		<View style={ styles.subView }>
-			{client.address.map((address, i) =>
-				<Text key={ i } style={ subtext }>
-					<Text style={ styles.light }>{address}</Text>
-				</Text>)}
-		</View>
+		<ClientAddress { ...context }/>
 	</View>;
 };
 
---
+++ src/components/ClientAddress.js
@@ -0,0 +1,23 @@
+import { View, Text } from '@react-pdf/renderer';
+import React from 'react';
+
+const subtext = {
+	marginLeft: 90,
+	paddingLeft: 30,
+	fontSize: 10,
+	width: 180,
+	flexWrap: 'wrap',
+};
+
+const ClientAddress = (context) => {
+	const { config: { obj: { client }}, data: styles } = context;
+
+	return <View style={ styles.subView }>
+		{client.address.map((address, i) =>
+			<Text key={ i } style={ subtext }>
+				<Text style={ styles.light }>{address}</Text>
+			</Text>)}
+	</View>;
+};
+
+export default ClientAddress;
