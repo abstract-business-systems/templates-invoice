fe082252cadf8760de9d34ce44741566111737cc
rkr-abs <rkr@abstractit.in>
2023-04-07 09:54:05 +0530

    Rename Invoice as index.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import './App.scss';
 import { PDFViewer, StyleSheet } from '@react-pdf/renderer';
-import Invoice from './components/Invoice/Invoice';
+import Invoice from './components/Invoice/';
 
 const styles = StyleSheet.create({
 	frame: {
