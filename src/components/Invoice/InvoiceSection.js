c267cfba67af4a2f8ba76414b394f9f6936172e6
rkr-abs <rkr@abstractit.in>
2023-04-12 12:42:23 +0530

    Implemented getInvoiceDetails service.

--- src/components/Invoice/InvoiceSection.js
+++ src/components/Invoice/InvoiceSection.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { View, Text } from '@react-pdf/renderer';
+import { map, values } from '@laufire/utils/collection';
 import InvoiceManager from '../../services/InvoiceManager';
 
 const InvoiceSection = (context) => {
-	const { config: { obj: { id }},
-		data: styles } = context;
-	const invoiceDate = InvoiceManager.getInvoiceDate(context);
+	const { data: styles } = context;
+	const invoiceDetails = InvoiceManager.getInvoiceDetails(context);
 
 	return <View style={ styles.view }>
-		<Text style={ styles.subtext }>
-			<Text style={ styles.bold }>Invoice No:</Text>
-			<Text style={ styles.light }> {id}</Text>
-		</Text>
-		<Text style={ [styles.subtext, styles.position] }>
-			<Text style={ styles.bold }>Invoice Date:</Text>
-			<Text style={ styles.light }> {invoiceDate}</Text>
-		</Text>
+		{values(map(invoiceDetails, (value, key) =>
+			<Text style={ styles.subtext }>
+				<Text style={ styles.bold }>{key}</Text>
+				<Text style={ styles.light }> {value}</Text>
+			</Text>))}
 	</View>;
 };
 
