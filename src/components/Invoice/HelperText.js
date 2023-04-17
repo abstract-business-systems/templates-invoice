51788410b79374c1f6a2ac607617fdac0b1b3399
rkr-abs <rkr@abstractit.in>
2023-04-17 18:13:16 +0530

    Rename Helper component.

--- src/components/Invoice/HelperText.js
+++ src/components/Invoice/HelperText.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Text } from '@react-pdf/renderer';
 
-const Helper = ({ styles, header, value }) =>
+const HelperText = ({ styles, header, value }) =>
 	<Text style={ styles.subtext }>
 		<Text style={ styles.bold }>{header}:</Text>
 		<Text style={ styles.light }>{ value}</Text>
 	</Text>;
 
-export default Helper;
+export default HelperText;
